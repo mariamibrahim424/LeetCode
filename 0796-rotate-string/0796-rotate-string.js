1c4e938129a84a795dac918d89b73bc5b818f6ed
Mariam Ibrahim <103009707+mariamibrahim424@users.noreply.github.com>
2024-02-18 02:26:24 -0800

    Time: 55 ms (46.36%), Space: 51.5 MB (5.50%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    let arr = s.split('');
+    for(const letter of s){
+        let first = arr.shift();
+        arr.push(first);
+        if(arr.join('') == goal){
+            return true;
+        }
+    }
+    return false;
+};
+
+// time complexity: O(n^2)
+// space complexity: O(n)
+
+// /**
+//  * @param {string} s
+//  * @param {string} goal
+//  * @return {boolean}
+//  */
+// var rotateString = function(s, goal) {
+//     let counter = s.length;
+//     let result = s;
+//     while(counter){
+//         let firstLet = result[0];
+//         result =  result.substring(1).concat(firstLet);
+//         console.log(result);
+//         if(result == goal)return true;
+//         counter--;
+//     }
+//     return false;
+// };
