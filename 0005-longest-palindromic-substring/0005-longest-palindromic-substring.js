aabaf4ec5ac0a7c7746a3e16a70fdbd53e94d93b
Mariam Ibrahim <103009707+mariamibrahim424@users.noreply.github.com>
2024-02-19 13:55:57 -0800

    Time: 2355 ms (5.02%), Space: 50.3 MB (58.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,8 +3,9 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    if(s.length === 1) return s;
+    // if(s.length === 1) return s;
     if(s.length === 0) return '';
+    
     let longestStr = s[0];
     for(let i = 0; i<s.length;i++){
         for(let j = s.length-1; j > i;j--){
