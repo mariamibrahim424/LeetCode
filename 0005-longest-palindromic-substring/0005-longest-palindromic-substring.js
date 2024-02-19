41bbca641e2d36985904ce51f88205741d6c5249
Mariam Ibrahim <103009707+mariamibrahim424@users.noreply.github.com>
2024-02-19 13:50:47 -0800

    Time: 2396 ms (5.02%), Space: 50.4 MB (57.40%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if(s.length === 1) return s;
+    if(s.length === 0) return '';
+    let longestStr = s[0];
+    for(let i = 0; i<s.length;i++){
+        for(let j = s.length-1; j > i;j--){
+            if(isPalindrome(i,j,s)){
+                if(j-i >= longestStr.length){
+                    longestStr = s.slice(i,j+1);
+                }
+            }
+        }
+    }
+    return longestStr;
+};
+
+var isPalindrome = function (start,end,str){
+    while(end > start){
+        if(str[end] !== str[start]) return false;
+        end--,start++;
+    }
+    return true;
+};
