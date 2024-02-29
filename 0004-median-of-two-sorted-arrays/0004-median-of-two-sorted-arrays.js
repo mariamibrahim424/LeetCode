29d62266cab7ef4fc13e2b6379390993ffa4139e
Mariam Ibrahim <103009707+mariamibrahim424@users.noreply.github.com>
2024-02-28 20:30:55 -0800

    Time: 105 ms (34.02%), Space: 54.9 MB (45.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -21,12 +21,8 @@ var findMedianSortedArrays = function(nums1, nums2) {
   result.push(...nums1.slice(i),...nums2.slice(j));
   
   let middle = Math.floor(result.length/2);
-  if(result.length % 2 == 0){
-    median = ((result[middle] + result[middle-1])/2)
-  }
-  else{
-    median = result[middle];
-  }
+  let isEven = result.length % 2 === 0;
+  median = isEven ? ((result[middle] + result[middle-1])/2) : result[middle];
   return median;
 };
 // time complexity: O(max(n,m)log(m+n))
