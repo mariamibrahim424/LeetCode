37d521c46803450a1881d934ef35b0d97619e2a4
Mariam Ibrahim <103009707+mariamibrahim424@users.noreply.github.com>
2024-02-28 20:28:25 -0800

    Time: 104 ms (36.37%), Space: 54.8 MB (47.35%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+ // time complexity: O(m+n) for traversing both arrays
+ // space complexity: O(m+n) for merged result array 
+var findMedianSortedArrays = function(nums1, nums2) {
+  let i = 0, j = 0, median = 0;
+  let result = [];
+  while(i < nums1.length && j <nums2.length){
+      if(nums1[i] < nums2[j]){
+          result.push(nums1[i]);
+          i++;
+      }
+      else{
+          result.push(nums2[j]);
+          j++;
+      }
+  }
+  result.push(...nums1.slice(i),...nums2.slice(j));
+  
+  let middle = Math.floor(result.length/2);
+  if(result.length % 2 == 0){
+    median = ((result[middle] + result[middle-1])/2)
+  }
+  else{
+    median = result[middle];
+  }
+  return median;
+};
+// time complexity: O(max(n,m)log(m+n))
+// var findMedianSortedArrays = function(nums1, nums2) {
+//     let median = 0;
+//     let result = [...nums1,...nums2];
+//     result.sort((a,b)=> a-b);
+//     let middleIndex = Math.floor(result.length/2);
+
+//     if(result.length % 2 ==0){
+//         median = ((result[middleIndex] + result[middleIndex-1])/2)
+//     }
+//     else{
+//         median = result[middleIndex]
+//     }
+//     return median
+// };
